61cdd1d11afa0ad70a41f7a619ac07f62dca034d
github username <github email address>
2019-03-07 04:38:02 -0500

    added card images

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 let scores = [0,0];
+let cardNumber = 1;
 
 
 document.querySelector('.button-hit').addEventListener('click', function(){
-    let randomNumber, card;
-    randomNumber = Math.floor(Math.random() * 13) + 1
-    if (randomNumber <= 10){
-      card = randomNumber;
-      scores[0] += card;
-    } else {
-      card = 10;
-      scores[0] += card;
-    }
-    document.querySelector('.player-first-card').textContent = card;
-    document.querySelector('.player-score').textContent = scores[0];
+    drawCard(0);
+    drawCard(1);
+    // document.querySelector('.player-first-card').textContent = card;
+    document.querySelector('.dealer-score').textContent = scores[0];
+    document.querySelector('.player-score').textContent = scores[1];
+    cardNumber += 1
 })
 
-// 1 = ace
-// 2 = 2
-// 3 = 3
-// 4 = 4
-// 5 = 5
-// 6 = 6
-// 7 = 7
-// 8 = 8
-// 9 = 9
-// 10 = 10
-// 11 = jack
-// 12 = queen
-// 13 = king
+function drawCard(player){
+  let randomNumber, card, cardDOM;
+  randomNumber = Math.floor(Math.random() * 52) + 1
+  if (randomNumber <= 4){
+    card = 1;
+    scores[player] += card;
+  } else if (randomNumber >=5 && randomNumber <= 8){
+    card = 2;
+    scores[player] += card;
+  }  else if (randomNumber >=9 && randomNumber <= 12){
+    card = 3;
+    scores[player] += card;
+  } else if (randomNumber >=13 && randomNumber <= 16){
+    card = 4;
+    scores[player] += card;
+  } else if (randomNumber >=17 && randomNumber <= 20){
+    card = 5;
+    scores[player] += card;
+  } else if (randomNumber >=21 && randomNumber <= 24){
+    card = 6;
+    scores[player] += card;
+  } else if (randomNumber >=25 && randomNumber <= 28){
+    card = 7;
+    scores[player] += card;
+  } else if (randomNumber >=29 && randomNumber <= 32){
+    card = 8;
+    scores[player] += card;
+  } else if (randomNumber >=33 && randomNumber <= 36){
+    card = 9;
+    scores[player] += card;
+  } else {
+    card = 10;
+    scores[player] += card;
+  }
+  cardDOM = document.getElementById('card-1-' + cardNumber);
+  cardDOM.src = './assets/' + randomNumber + '.png';
+}
